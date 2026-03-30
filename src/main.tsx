f384366f269af5e1e5bc813c7140bd6831fc7e93
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2026-03-30 12:26:01 -0300

    fix: update Vercel Analytics import to use the correct package

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
